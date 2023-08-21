89168a92af89ff707289a23a3af69003ce055103
fox-takata <takemasa.takata@fox-hound.jp>
2023-08-21 18:55:09 +0900

    fix: ツールチップ表示

--- src/main/java/com/example/repository/CategoryProductRepository.java
+++ src/main/java/com/example/repository/CategoryProductRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.model.CategoryProduct;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoryProductRepository extends JpaRepository<CategoryProduct, Long> {
 	void deleteByCategoryId(Long categoryId);
@@ -12,4 +13,9 @@ public interface CategoryProductRepository extends JpaRepository<CategoryProduct
 	List<CategoryProduct> findByProductId(Long productId);
 
 	List<CategoryProduct> findByCategoryId(Long categoryId);
+
+	// カテゴリIDで紐づく商品の名前のリストを返却
+	@Query("SELECT p.product.name FROM CategoryProduct p WHERE p.categoryId = ?1")
+	List<String> findProductNamesByCategoryId(Long categoryId);
+
 }
--- src/main/resources/static/js/category/category-index.js
+++ src/main/resources/static/js/category/category-index.js
@@ -1,10 +1,67 @@
-$(document).load(function () {
-  var tooltipTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  );
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl, {
-      trigger: "click",
+// $(document).load(function () {
+//   var tooltipTriggerList = [].slice.call(
+//     document.querySelectorAll('[data-bs-toggle="tooltip"]')
+//   );
+//   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//     return new bootstrap.Tooltip(tooltipTriggerEl, {
+//       trigger: "click",
+//     });
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var clickableIcons = document.querySelectorAll('.clickable-tooltip-icon');
+  var tooltipInstance = null; // ツールチップのインスタンスを保持する変数
+
+  // ドキュメント全体のクリックイベントを監視
+  document.addEventListener('click', function (event) {
+    // クリックされた要素がボタンでない場合はツールチップを閉じる
+    if (!event.target.classList.contains('clickable-tooltip-icon') && tooltipInstance) {
+      tooltipInstance.dispose();
+      tooltipInstance = null;
+    }
+  });
+
+  clickableIcons.forEach(function (icon) {
+    icon.addEventListener('click', function () {
+      if (!tooltipInstance) {
+        // ツールチップが存在しない場合は新しいツールチップを作成
+        var categoryId = icon.getAttribute('data-category-id');
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/api/categories/' + categoryId + '/categoryNames', true);
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+              var categoryNames = JSON.parse(xhr.responseText);
+              var tooltipContent = '現在紐づいている商品一覧';
+              tooltipContent += '<ul>';
+              if (categoryNames.length == 0) {
+                tooltipContent += 'ありません';
+              }
+              for (var i = 0; i < categoryNames.length; i++) {
+                tooltipContent += '<li>' + categoryNames[i] + '</li>';
+              }
+              tooltipContent += '</ul>';
+
+              tooltipInstance = new bootstrap.Tooltip(icon, {
+                title: tooltipContent,
+                html: true,
+                placement: 'top',
+                trigger: 'manual'
+              });
+
+              tooltipInstance.show(); // ツールチップを表示
+            } else {
+              console.error('Error: ' + xhr.status);
+            }
+          }
+        };
+        xhr.send();
+      } else {
+        // ツールチップが既に存在する場合は閉じる
+        tooltipInstance.dispose();
+        tooltipInstance = null; // ツールチップのインスタンスをクリア
+      }
     });
   });
 });
